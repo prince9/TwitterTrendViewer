99f17a73e8c640e9685e44c2aaa422ea12a1334b
MAYU <mtsuzaka@rb3.so-net.ne.jp>
2012-05-19 08:45:05 +0900

    Add Comment.

--- TwitterTrendView/ViewController.h
+++ TwitterTrendView/ViewController.h
@@ -8,6 +8,22 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIActionSheetDelegate> {
+    
+    //トレンドのリスト
+    NSMutableArray *trendArray;
+    //トレンドのリストの0番目・1番目・2番目・3番目のデータの文字列
+    NSString *trendname1,*trendname2,*trendname3,*trendname4;
+ 
+}
 
+
+
+@property (weak, nonatomic) IBOutlet UILabel *trendLabel;
+@property (nonatomic,retain) NSMutableArray *trendArray;
+
+@property (nonatomic,retain) NSString *trendname1,*trendname2,*trendname3,*trendname4;
+
+//アクションシートを起動
+- (IBAction)trendAction:(id)sender;
 @end
--- TwitterTrendView/ViewController.m
+++ TwitterTrendView/ViewController.m
@@ -13,17 +13,54 @@
 @end
 
 @implementation ViewController
+@synthesize trendLabel;
+@synthesize trendArray;
+@synthesize trendname1,trendname2,trendname3,trendname4;
+
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    //取得するトレンドの数を最高4個にする
+    trendArray = [[NSMutableArray alloc] initWithCapacity:4];
+    
+    //トレンドのURLを呼び出す
+    NSURL *twTurl = [NSURL URLWithString:@"https://api.twitter.com/1/trends/23424856.json"];
+     //設定したアドレスにアクセスする設定をする
+    NSURLRequest *twTrequest = [NSURLRequest requestWithURL:twTurl];
+     //NSURLConnectionで実際にアクセスする
+    [NSURLConnection sendAsynchronousRequest:twTrequest queue:[NSOperationQueue mainQueue]completionHandler:^(NSURLResponse *twTresponse, NSData *twTdata, NSError *twTerror) {
+        if (twTerror) {
+            NSLog(@"error: %@", [twTerror localizedDescription]);
+            return;
+        }
+        
+         //jsonで解析する。NSJSONSerializationはiOS5から使える
+        NSArray *json = [NSJSONSerialization JSONObjectWithData:twTdata
+                                                        options:kNilOptions 
+                                                          error:&twTerror];
+        //ここSearchAPIと異なるので注意!
+        trendArray = [[json objectAtIndex:0] objectForKey:@"trends"];
+        NSLog(@"%@",trendArray);
+         //nameキーの上から4つを取り出す
+       trendname1 = [[trendArray objectAtIndex:0] objectForKey:@"name"];         
+         trendname2 = [[trendArray objectAtIndex:1] objectForKey:@"name"];
+       trendname3 = [[trendArray objectAtIndex:2] objectForKey:@"name"];
+        trendname4 = [[trendArray objectAtIndex:3] objectForKey:@"name"];
+            }];
+  
 }
+     
 
 - (void)viewDidUnload
 {
+    [self setTrendLabel:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
+    
+    
+    
 }
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
@@ -31,4 +68,20 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+//アクションシートを起動する
+- (IBAction)trendAction:(id)sender {
+    //タイトルやキャンセルボタンの設定、destructiveButtonTitleはシートのいちばん上のボタンに表示する文字列でotherButtonTitlesは2番目以降
+    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"トレンドリスト" delegate:self cancelButtonTitle:@"キャンセル" destructiveButtonTitle:trendname1 otherButtonTitles:trendname2,trendname3,trendname4, nil];
+    //アクションシートを表示する
+    [actionSheet showInView:self.view];
+    
+}
+
+//アクションシートのボタンをタップしたときにする処理
+-(void)actionSheet:(UIActionSheet *)actionSheet clickedButtonAtIndex:(NSInteger)buttonIndex {
+    //タップしたボタンに書いてある文字列を表示
+    trendLabel.text = [actionSheet buttonTitleAtIndex:buttonIndex];
+    
+    
+}
 @end
